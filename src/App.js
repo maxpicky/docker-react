21b6ce9a04a36456cc191128d59e7965f6455771
Maxime PIOC <maxime.pioc@havasdigitalfactory.com>
2020-04-08 15:56:04 +0200

    new update

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hey <code>src/App.js</code> regis est un BG.
+          Hey <code>src/App.js</code> regis est un BG - MAJ FROM DEV
         </p>
         <a
           className="App-link"
